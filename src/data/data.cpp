bbb72a28c87d66971a1bd591a10e8e8fc342e71e
Young, Todd <youngmt1@ornl.gov>
2019-12-05 21:03:04 -0500

    Add doctstrings
    This clarifies the intent of the code.

--- src/data/data.cpp
+++ src/data/data.cpp
@@ -1,5 +1,3 @@
-#include <vector>
-#include <tuple>
 #include <torch/torch.h>
 #include "data.hpp"
 
@@ -14,24 +12,29 @@ constexpr uint32_t kTrainSize = 60000;
 
 Epath::Epath(Mode mode)
     : text_(load_text()),
-      targets_(load_labels()) {};
+      targets_(load_targets()) {};
 
+/// Get a batch of data at an index
 torch::data::Example<> Epath::get(size_t index) {
-    return {text_[index], labels_[index]};
+    return {text_[index], targets_[index]};
 }
 
+/// Get the size of the dataset
 torch::optional<size_t> Epath::size() const {
     return text_.size(0);
 }
 
+/// Check whether this is the train set
 bool Epath::is_train() const noexcept {
     return text_.size(0) == kTrainSize;
 }
 
+/// Return all of the text data as a single Pytorch tensor
 const torch::Tensor& Epath::text() const {
     return text_;
 }
 
+/// Return all of the labels as a single Pytorch tensor
 const torch::Tensor& Epath::targets() const {
     return targets_;
 }
