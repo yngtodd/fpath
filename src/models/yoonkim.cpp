5185892039404637301fb9458bc91b073f5a78cb
yngtodd <young.todd.mk@gmail.com>
2019-12-17 11:40:55 -0500

    Add layer declaration, params
    We can now specify the various hyperparameters of the model.

--- src/models/yoonkim.cpp
+++ src/models/yoonkim.cpp
@@ -7,43 +7,53 @@ using namespace torch;
 namespace fpath {
 namespace models {
 
-/// Yoon Kim CNN for text classification
-/// 
+/** 
+ * Yoon Kim CNN for text classification
+ *
+ * @param num_embeddings: number of tokens to embed
+ * @param embedding_dim: dimension of the word embeddings 
+ * @param num_filters: number of convolution filters per layer
+ */
 struct YoonKim : torch::nn::Module {
 
-    YoonKim() {
+    YoonKim(int64_t num_embeddings, int64_t embedding_dim, int64_t num_filters) {
 	embed = register_module(
              "embed",
 	     nn::Embedding(nn::EmbeddingOptions(
-	          /*num_embeddings=*/1000,
-		  /*embedding_dim=*/300))
+	          /*num_embeddings=*/num_embeddings,
+		  /*embedding_dim=*/embedding_dim))
         );
 
         conv1 = register_module(
 	    "conv1", 
 	    nn::conv1d(nn::Conv1dOptions(
-	         /*in_channels=*/300,
-		 /*out_channels=*/3))
+	         /*in_channels=*/embedding_dim,
+		 /*out_channels=*/num_filters))
 	);
         
 	conv2 = register_module(
 	    "conv2", 
             nn::conv1d(nn::Conv1dOptions(
-	         /*in_channels=*/300,
-		 /*out_channels=*/3))
+	         /*in_channels=*/embedding_dim,
+		 /*out_channels=*/num_filters))
 	);
 
         conv3 = register_module(
 	    "conv3", 
             nn::conv1d(nn::Conv1dOptions(
-	         /*in_channels=*/300,
-		 /*out_channels=*/3))
+	         /*in_channels=*/embedding_dim,
+		 /*out_channels=*/num_filters))
 	);
 
 	fc = register_module(
 	    "fc",
-	    nn::Linear(/*input_dim=*/0, /*output_dim=*/0)
+	    nn::Linear(/*input_dim=*/num_filters * 3, /*output_dim=*/10)
 	);
+
+	// declare all layers
+	nn::Embedding{nullptr};
+	nn::Conv1d conv1{nullptr}, conv2{nullptr}, conv3{nullptr};
+	nn::Linear{nullptr};
     }
 
     torch::Tensor relu_pool(torch::Tensor x) {
@@ -56,7 +66,7 @@ struct YoonKim : torch::nn::Module {
         auto x1 = relu_pool(conv1(embedding)); 
 	auto x2 = relu_pool(conv2(embedding));
 	auto x3 = relu_pool(conv3(embedding));
-	auto x = torch::cat([x1, x2, x3], /*dim=*/0);
+	auto x = torch::cat({x1, x2, x3}, /*dim=*/0);
 	return fc(x);
     }
 
