c8c1455dc11f92a78fea1459a698bcd74df0f9c6
yngtodd <young.todd.mk@gmail.com>
2019-12-17 14:28:37 -0500

    Fix missing bracket
    Gonna need that.

--- src/models/models.hpp
+++ src/models/models.hpp
@@ -9,14 +9,17 @@ namespace fpath {
 namespace models {
 
 struct YoonKim: torch::nn::Module {
-    
+
     YoonKim(int64_t num_embeddings, int64_t embedding_dim, int64_t num_filters);
 
+    // ReLu => max pool 1D
     torch::Tensor relu_pool(torch::Tensor x);
 
-    int64_t total_filters(int64_t num_filters);
-
     torch::Tensor forward(const torch::Tensor& input);
+
+    // Total number of filters for the model
+    int64_t total_filters(int64_t num_filters);
+}
 
 } // models
 } // fpath
